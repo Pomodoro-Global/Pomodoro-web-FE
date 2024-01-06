4018896f74ec054a9b44c9254aa124c7eea3aec6
m-aminalizadeh <aminalizadeh@riseup.net>
2024-01-06 11:50:58 +0330

    Add Tailwind className to the header one

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 function App() {
-
   return (
-    <>
-      Hello world
-    </>
-  )
+    <h1 className="text-4xl">
+      Pomodoro App
+    </h1>
+  );
 }
 
-export default App
+export default App;
