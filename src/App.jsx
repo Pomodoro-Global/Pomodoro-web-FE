067a07f76fe87e0f9c8962ab997c3f1f9b961694
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2024-02-18 15:57:43 +0330

    Import navbar to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <h1 className="text-4xl">
-      Pomodoro App
-    </h1>
+    <>
+      <Navbar />
+    </>
   );
 }
 
