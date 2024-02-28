81d2d7c5c5d8a117f9069a4e403dc40ce55a226f
m-aminalizadeh <aminalizadeh@riseup.net>
2024-02-28 23:55:56 +0330

    Add functionality to show the reverse time counting per second on the console

--- src/components/pomodoro-circle/Timer.jsx
+++ src/components/pomodoro-circle/Timer.jsx
@@ -1,3 +1,32 @@
-const Timer = () => (<span className="font-black text-6xl"> 25:00 </span>);
+import { useState, useEffect } from 'react';
+
+const Timer = () => {
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    // handling seconds
+    // const handleSeconds = () => {
+    //   if (seconds === '00') {
+    //     console.log('inside condition');
+    //     for (let s = 59; s >= 0; s -= 1) {
+    //       setTimeout(setSeconds(s) ,1000);
+    //     }
+    //   }
+    // };
+    // handleSeconds();
+    setTimeout(() => {
+      
+    }, 1000);
+    // handling minutes
+  }, []);
+  return (
+    <div className="font-black text-6xl">
+      <span>{minutes}</span>
+      :
+      <span>{seconds}</span>
+    </div>
+  );
+};
 
 export default Timer;
